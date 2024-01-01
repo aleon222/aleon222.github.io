1a7b0cc8c36ecad54ee546b188b0c963396333a5
alyssa colette <136546965+aleon222@users.noreply.github.com>
2024-01-01 13:30:43 -0500

    Header active states

--- rrHeader.js
+++ rrHeader.js
@@ -7,8 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
         headerContainer.innerHTML = xhr.responseText;
-      }
-    };
-    xhr.send();
-  });
-  
+      
+      //update script 1.1.24
+
+  //get all <a> elements
+  var links = document.querySelectorAll('nav .mainMenu a');
+
+  //loop through the links
+  for (var i=0; i < links.length; i++){
+    //check if current page matches href of the link
+    if (window.location.href.endsWith(links[i].getAttribute('href'))){
+      //add active class to link and its parent .mainMenu div
+      links[i].classList.add('active');
+      // links[i].closest('.mainMenu').classList.add('active');
+
+       // If the link is part of a dropdown, also add the active class to its parent .dropdown div
+        var parentDropdown = links[i].closest('.dropdown');
+        if (!parentDropdown) {
+          links[i].closest('.mainMenu').classList.add('active');
+        }
+    }
+  }
+}
+};
+  xhr.send();
+});
